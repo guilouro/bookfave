b132729459bd1c808c7583d7fc3232ff5ff47560
Guilherme Louro <guilherme-louro@hotmail.com>
2019-05-09 09:47:48 -0300

    Add assets

---
+++ src/reducers/favorites/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  entities: {},
+  allIds: []
+}
+
+const favorites = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default favorites
